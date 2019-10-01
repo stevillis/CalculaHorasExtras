ee6c7cac5ebf811675e9a233b246e4905601ac64
stevillis <stevillis@hotmail.com>
2019-09-30 23:20:12 -0400

    Adicionado alerta quando nenhum match é encontrada na busca de horas extras no texto

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
 function calculaHoras() {
     const textAreaTexto = document.getElementById("validationTextarea");
-    const divAlerta = document.getElementById("res");    
+    const divRes = document.getElementById("res");
+    const divAlerta = document.getElementById("alerta");
+
     const regex = /\d{2}:\d{2}/gm;
-    let arrayHoras = textAreaTexto.value.match(regex);    
+    let arrayHoras = textAreaTexto.value.match(regex);
 
-    if (arrayHoras.length > 0) {
-        const divRes = document.getElementById("res");
-        const spanDias = document.getElementById("dias");
-        const spanHoras = document.getElementById("horas");
+    if (arrayHoras) {
+        if (arrayHoras.length > 0) {
+            const spanDias = document.getElementById("dias");
+            const spanHoras = document.getElementById("horas");
 
-        let minutos = 0;
-        let horas = 0;
-        let horasTotais = 0;
+            let minutos = 0;
+            let horas = 0;
+            let horasTotais = 0;
 
-        for (let minString of arrayHoras) {
-            horas += parseInt(minString.slice(0, 2));
-            minutos += parseInt(minString.slice(3));
+            for (let minString of arrayHoras) {
+                horas += parseInt(minString.slice(0, 2));
+                minutos += parseInt(minString.slice(3));
+            }
+
+            horasTotais = horas * 60 + minutos;
+
+            divAlerta.style.opacity = 0;
+            divRes.style.opacity = 1;
+
+            spanDias.innerText = String(arrayHoras.length);
+            spanHoras.innerText = `${Math.floor(horasTotais / 60)}:${horasTotais % 60}`;
         }
-
-        horasTotais = horas * 60 + minutos;
-
-        divAlerta.style.opacity = 0;
-        divRes.style.opacity = 1;
-
-        spanDias.innerText = String(arrayHoras.length);
-        spanHoras.innerText = `${Math.floor(horasTotais / 60)}:${horasTotais % 60}`;
-    } else {        
+    } else {
         divRes.style.opacity = 0;
         divAlerta.style.opacity = 1;
     }
